5e8d21c6fb5c8d1852334ff375c1bb7e8feaa22f
Oleg <oleg.dryukov@gmail.com>
2025-07-24 12:53:39 +0400

    Creating workshop-page.js

---
+++ pages/scripts/workshop-page.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('.js-home-icon')
+  .forEach((icon) => {
+    icon.addEventListener('click', () => {
+    window.location.href = 'main-page.html';
+  });
+})
+
+document.querySelectorAll('.js-shop-button')
+  .forEach((shopButton) => {
+    shopButton.addEventListener('click', () => {
+      window.location.href = 'shop-page.html'
+    });
+  })
+
+document.querySelectorAll('.js-workshop-button')
+.forEach((workshopButton) => {
+  workshopButton.addEventListener('click', () => {
+    window.location.href = 'workshop-page.html'
+  });
+})
